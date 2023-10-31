049e5bd7db45f7431a17c0a743811ca7cd681dba
Matthias Gielen <matthi_gielen@hotmail.be>
2023-10-31 12:42:40 +0100

    Updates in the index.blade.php file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
+
 @extends('master')
 
 @section('title', 'Welkom bij Webcrafters')
@@ -5,5 +6,27 @@
 @section('description', 'Waar uw idee digitaal gaat')
 
 @section('content')
+    <div class="services">
+        <h1 class="w-100">Welkom bij Webcrafters</h1>
+        <p>Wij bieden een breed scala aan diensten om aan uw behoeften te voldoen. Hier zijn enkele van de diensten die wij aanbieden:</p>
+        <div class="blocks">
+            <div class="block shadow">
+                <h4>Webontwikkeling</h4>
+            </div>
+            <div class="block shadow">
+                <h4>Applicatie-ontwikkeling</h4>
+            </div>
+            <div class="block shadow">
+                <h4>Website-ontwerp</h4>
+            </div>
+            <div class="block shadow">
+                <h4>Digitale marketing</h4>
+            </div>
+            <div class="block shadow">
+                <h4>Consulting</h4>
+            </div>
+        </div>
 
+        <p>Neem contact met ons op voor meer informatie over onze diensten of om een project te bespreken.</p>
+    </div>
 @endsection
